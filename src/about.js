0f6e2c14fa78f8b0438fe6354cac8b7bda999674
Manu BuddHa <ferano915@gmail.com>
2025-03-21 17:21:37 +0530

    Ready to Publish

--- src/about.js
+++ src/about.js
@@ -3,20 +3,8 @@ function about(){
    const heading = document.createElement("h1");
    const headDiv = document.createElement("div");
    const content = document.createElement("div");
-   const images = document.createElement("div");
-   const text = document.createElement("div");
    const p1 = document.createElement("p");
    p1.textContent = "Welcome to the about section of this Page. Here should be About!";
-   text.appendChild(p1);
-   images.classList.add("imageDiv");
-   const img1 = document.createElement("img");
-   const img2 = document.createElement("img");
-   const img3 = document.createElement("img");
-   img1.src="./odin-lined.png";
-   img1.alt="No Image";
-   img1.classList.add("images");
-   img2.classList.add("images");
-   img3.classList.add("images");
    content.classList.add("content");
    headDiv.classList.add("headDiv");
    heading.textContent = "About";
@@ -24,10 +12,6 @@ function about(){
    mainContent.appendChild(headDiv);
    mainContent.appendChild(content);
    headDiv.appendChild(heading);
-   content.appendChild(images);
-   content.appendChild(text);
-   images.appendChild(img1);
-   images.appendChild(img2);
-   images.appendChild(img3);
+   content.appendChild(p1);
 }
 export {about};
